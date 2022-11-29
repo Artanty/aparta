6027f354975815088fdb897c1b4b3eac8adb648e
Artyom <thomasway7@gmail.com>
2022-11-29 23:31:29 +0300

    gitActions

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://aparta.tk/api',
+  version: 'T_TAG_T'
+};
---
+++ frontend/src/environments/environment.tpl.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:8000/api',
+  version: 'T_TAG_T'
+};
