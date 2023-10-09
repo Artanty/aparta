b65689db9e21e626264461b0f215f0d2844c514e
Artyom <thomasway7@gmail.com>
2023-10-09 14:13:12 +0300

    lib input fix

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -44,7 +44,7 @@ import { CurrancyTableCellComponent } from './features/currancy-table-cell/curra
 import { CurrencyOnlyPipe } from './pipes/currency-only.pipe';
 import { SelectComponent } from './components/form/select/select.component';
 import { SwitchComponent } from './components/form/switch/switch.component';
-import { NgAlibiModule, LibraryConfig } from '@ng-alibi';
+import { NgAlibiModule, LibraryConfig } from 'ng-alibi';
 import { NgAlibiIconsModule, NgAlibiIconsRegistry, dinosaurIconsArtist, dinosaurIconsBirthday, dinosaurIconsChef, dinosaurIconsSleep, dinosaurIconsSpace, left, right
 } from 'ng-alibi-icons';
 import { LocaleService } from './services/locale/locale.service'
